691124a600eb231971e79fb252ddbf9e881cfd18
joni1612 <58235171+joni1612@users.noreply.github.com>
2021-01-25 15:58:36 +0100

    first commit before sidebar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+import ToDoList from "./components/ToDo";
+import RandomQuote from "./components/RandomQuote";
+import DailyImage from "./components/DailyImage";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Sidebar />
+      <RandomQuote />
+      <ToDoList />
+      <DailyImage />
     </div>
   );
 }
